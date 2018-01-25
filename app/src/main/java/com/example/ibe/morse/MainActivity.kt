4282d8b4817b5f59db8265dac0cdee3acc4f039d
Ibrahem Alahdal <ibralah@ut.utm.edu>
2018-01-25 09:12:32 -0600

    Code

--- app/src/main/java/com/example/ibe/morse/MainActivity.kt
+++ app/src/main/java/com/example/ibe/morse/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.ibe.morse
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.text.method.ScrollingMovementMethod
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +20,12 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+        mTextView.movmentMethod = ScrollingMovementMethod();
+
+        testButton.setOnClickListener { view ->
+            appendTextAndScroll(inputText.text.toString());
+            hideKeyboard();
         }
     }
 
